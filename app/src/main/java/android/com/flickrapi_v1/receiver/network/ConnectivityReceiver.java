b63ec1fca8869b8b531664b368f02e9357c7cbf4
Ziad <zi.eus@hotmail.com>
2020-05-09 22:55:46 +0200

    Add Swipe functionality

--- app/src/main/java/android/com/flickrapi_v1/receiver/network/ConnectivityReceiver.java
+++ app/src/main/java/android/com/flickrapi_v1/receiver/network/ConnectivityReceiver.java
@@ -1,17 +1,9 @@
 package android.com.flickrapi_v1.receiver.network;
-import android.com.flickrapi_v1.R;
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.util.Log;
-
-import androidx.appcompat.app.AlertDialog;
-
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class ConnectivityReceiver extends BroadcastReceiver {
 
@@ -37,31 +29,3 @@ public class ConnectivityReceiver extends BroadcastReceiver {
         void onNetworkConnectionChanged(boolean isConnected);
     }
 }
-//public class NetworkChangeReceiver extends BroadcastReceiver {
-
-//    private static final String TAG = "NetworkChangeReceiver";
-//    AlertDialog showAndhide;
-//
-//    @Override
-//    public void onReceive(Context context, Intent intent) {
-//        if (isOnline(context)) {
-//            showAndhide = null;
-//        } else {
-//            showAndhide = null;
-//            showAndhide = new MaterialAlertDialogBuilder(context, R.style.ThemeOverlay_App_MaterialAlertDialog)
-//                    .setTitle(R.string.network_not_available)
-//                    .setMessage(R.string.network_not_available_message)
-//                    .setOnDismissListener(dialog -> showAndhide.dismiss())
-//                    .show();
-//        }
-//    }
-//
-//    private boolean isOnline(Context context) {
-//        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-//        NetworkInfo activeNetworkInfo = null;
-//        if (cm != null) {
-//            activeNetworkInfo = cm.getActiveNetworkInfo();
-//        }
-//        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
-//    }
-//}
