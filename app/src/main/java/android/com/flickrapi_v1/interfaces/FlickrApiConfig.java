95bde24ee739295802c2f04bafb519c5a185450a
Ziad <zi.eus@hotmail.com>
2020-05-16 01:15:09 +0200

    Add retrofit Search

--- app/src/main/java/android/com/flickrapi_v1/interfaces/FlickrApiConfig.java
+++ app/src/main/java/android/com/flickrapi_v1/interfaces/FlickrApiConfig.java
@@ -1,8 +1,5 @@
 package android.com.flickrapi_v1.interfaces;
-
 import android.com.flickrapi_v1.pojo._PhotoModel;
-
-import java.util.List;
 
 import io.reactivex.rxjava3.core.Single;
 import retrofit2.Call;
@@ -20,10 +17,20 @@ public interface FlickrApiConfig {
             ,@Query("nojsoncallback") int nojsoncallback);
 
     @GET("rest/")
-    Call<_PhotoModel> getRecentPhotoPage(@Query("method") String method
+    Call<_PhotoModel> getRecentPhotoPage(
+            @Query("method") String method
             ,@Query("api_key") String api_key
             ,@Query("extras") String extras
             ,@Query("page") int page
+            ,@Query("format") String format
+            ,@Query("nojsoncallback") int nojsoncallback);
+
+    @GET("rest/")
+    Call<_PhotoModel> getPhotoSearch(
+            @Query("method") String method
+            ,@Query("api_key") String api_key
+            ,@Query("text") String text
+            ,@Query("extras") String extras
             ,@Query("format") String format
             ,@Query("nojsoncallback") int nojsoncallback);
 }
