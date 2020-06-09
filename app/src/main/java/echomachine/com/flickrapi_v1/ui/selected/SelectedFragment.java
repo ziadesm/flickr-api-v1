2267f77512bbe96f7a8d4b024811da7b5f97ae4d
Ziad <zi.eus@hotmail.com>
2020-06-09 18:01:06 +0200

    Image layout

--- app/src/main/java/echomachine/com/flickrapi_v1/ui/selected/SelectedFragment.java
+++ app/src/main/java/echomachine/com/flickrapi_v1/ui/selected/SelectedFragment.java
@@ -8,11 +8,17 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import echomachine.com.flickrapi_v1.R;
 import echomachine.com.flickrapi_v1.receiver.network.ConnectivityReceiver;
 
@@ -21,6 +27,10 @@ public class SelectedFragment extends Fragment {
     private static final String TAG = "ZiadSelected";
     private SelectedViewModel viewModel;
     private NavController navController;
+    private FloatingActionButton mChooseFab, mDownloadFab, mShareFab, mWallpaperFab;
+    private ImageView mImageViewer;
+    private RecyclerView mRecycler;
+    private TextView mTagsTextView;
 
     public SelectedFragment() {
     }
@@ -33,7 +43,6 @@ public class SelectedFragment extends Fragment {
             @Override
             public void handleOnBackPressed() {
                 // Handle the back button event
-                Log.d(TAG, "HEEEEERRRREEEEE");
                 navController.navigateUp();
             }
         };
@@ -46,6 +55,14 @@ public class SelectedFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_selected, container, false);
+
+        mChooseFab = view.findViewById(R.id.selected_floating_action_btn);
+        mDownloadFab = view.findViewById(R.id.selected_download_floating_action_btn);
+        mShareFab = view.findViewById(R.id.selected_share_floating_action_btn);
+        mWallpaperFab = view.findViewById(R.id.selected_set_as_wallpaper_floating_action_btn);
+        mImageViewer = view.findViewById(R.id.selected_image_header);
+        mRecycler = view.findViewById(R.id.selected_recycler_view);
+        mTagsTextView = view.findViewById(R.id.selected_tags_appear);
 
         viewModel = ViewModelProviders
                 .of(this).get(SelectedViewModel.class);
